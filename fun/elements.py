5ccca2c8682e0a0ae506282eba7157ac1aad2040
Simon Potter <scp@ebi.ac.uk>
2022-11-25 14:55:02 +0000

    Split string into valid chemical element symbols

---
+++ fun/elements.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Credit for finding all ways to split a string:
+#https://www.geeksforgeeks.org/find-all-possible-ways-to-split-the-given-string-into-primes/
+
+import sys
+
+if len(sys.argv) != 2:
+    sys.exit(1)
+word = sys.argv[1]
+
+elements = {}
+with open('elements.csv') as fh:
+    for line in fh:
+        symbol = line.rstrip()
+        elements[symbol.lower()] = symbol
+
+wlen = len(word)
+nsplits = 2 ** (wlen - 1)
+
+for i in range(nsplits):
+    idx = 0
+    splitc = []
+    for j in range(wlen):
+        splitc.append(word[j])
+        splitc.append(" " if i & 1 << j else "")
+    splitc.pop()
+    words = ''.join(splitc).split(' ')
+    words = [elements[x] if x in elements else "" for x in words]
+    if '' in words:
+        continue
+    print('-'.join(words))
